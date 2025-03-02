f0ac57b9dd69da94bb903ee56072f650503bf8f2
mehedii-h0 <mehedii.h03@gmail.com>
2025-03-02 16:56:16 +0600

    grid layout for square section change

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -38,7 +38,7 @@ const Projects = () => {
             case "landscape":
                 return "grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2";
             case "portrait":
-                return "grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4";
+                return "grid-cols-1 sm:grid-cols-2 md:grid-cols-3";
             case "square":
             default:
                 return "grid-cols-1 sm:grid-cols-2 md:grid-cols-3";
